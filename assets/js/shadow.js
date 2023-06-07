b2e6e8d108350721c1338d23c57e9553c3274225
jobs.bezu@gmail.com <jobs.bezu@gmail.com>
2023-06-07 18:52:43 +0300

    staging

--- assets/js/shadow.js
+++ assets/js/shadow.js
@@ -16,13 +16,9 @@ customElements.define('contactus-component', class extends HTMLElement {
 
       <h2 class="h2 section-title">Let's Contact With Us</h2>
 
-      <p class="section-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-        dolore magna
-        aliqua.
-      </p>
+      
 
-      <form action="" class="contact-form">
+      <form style="margin-top: 20px;"action="" class="contact-form">
 
         <div class="input-wrapper">
           <input type="text" name="name" aria-label="name" placeholder="Your name*" required class="input-field">
@@ -40,14 +36,7 @@ customElements.define('contactus-component', class extends HTMLElement {
         <textarea name="message" aria-label="message" placeholder="Your message...*" required
           class="input-field"></textarea>
 
-        <div class="checkbox-wrapper">
-          <input type="checkbox" name="terms_and_policy" id="terms" required class="checkbox">
-
-          <label for="terms" class="label">
-            Accept <a href="#" class="label-link">Terms of Services</a> and <a href="#" class="label-link">Privacy
-              Policy</a>
-          </label>
-        </div>
+        
 
         <button type="submit" class="btn btn-primary">Send Message</button>
 
@@ -66,8 +55,8 @@ customElements.define('contactus-component', class extends HTMLElement {
 
               <h3 class="h3 card-title">Mail Here</h3>
 
-              <a href="mailto:hello@luaz.com" class="card-link">hello@luaz.com</a>
-              <a href="mailto:info@luaz.com" class="card-link">info@luaz.com</a>
+              <a href="mailto:hello@luaz.com" class="card-link">Prophetmaranatashimelis@gmail.com</a>
+              <a href="mailto:info@luaz.com" class="card-link">info@ChristLoveTv.com</a>
 
             </div>
 
@@ -86,8 +75,8 @@ customElements.define('contactus-component', class extends HTMLElement {
               <h3 class="h3 card-title">Visit Here</h3>
 
               <address class="card-address">
-                27 Division St, New York,<br>
-                NY 10002, USA
+              አዲስ አበባ ከአያት አደባባይ ወደፀበል በሚወስደው መንገድ <br>
+              አዲስ መንደር ቤቶች ፊትለፊት ንግድ ባንክ ጎን ሾቤ አያት ቅርንጫፍ
               </address>
 
             </div>
@@ -106,8 +95,8 @@ customElements.define('contactus-component', class extends HTMLElement {
 
               <h3 class="h3 card-title">Call Here</h3>
 
-              <a href="tel:+1234567890" class="card-link">+123 456 7890</a>
-              <a href="tel:+2414524526" class="card-link">+241 452 4526</a>
+              <a href="tel:+1234567890" class="card-link">+251 911 803799</a>
+              <a href="tel:+2414524526" class="card-link">+251 901 901010</a>
 
             </div>
 
@@ -157,15 +146,15 @@ customElements.define('aboutus-component', class extends HTMLElement {
 
     <div class="about-content">
 
-      <h2 class="h2 section-title">About Us</h2>
+      
 
-      <p class="section-text">
+      <p style="font-size: 2.0rem;" class="section-text">
       የእግዚአብሔር ታላቅነት መገለጫ ቤተክርስቲያን በኢትዮጵያና ከኢትዮጵያ ውጭ የሚደረጉ የወንጌል አገልግሎቶችን እንድትደግፉ በጌታ ፍቅር ጥሪ ታቀርባለች።
       </p>
 
       <!--<h3 class="h3">Who We Are</h3>-->
 
-      <p class="section-text">
+      <p style="font-size: 2.0rem;" class="section-text">
       በጌታ የተወደዳችሁ የክርስቶስ ቤተሰቦች: ባለፉት ጥቂት አመታት በዚህ ወንጌል አገልግሎት ከቤተ ክርስቲያን ጋር አብራችሁ በገንዘባችሁ፣ 
       በምክራችሁ ፣በፀሎታችሁ ፣በጉልበታችሁ ከጎናችን የቆማችሁ ሁላችሁንም እግዚአብሄር አምላክ አብዝቶ ይባርካችሁ።
       </p>
@@ -177,7 +166,7 @@ customElements.define('aboutus-component', class extends HTMLElement {
         <li class="about-item">
           <ion-icon name="checkmark-circle" aria-hidden="true"></ion-icon>
 
-          <p class="section-text">
+          <p style="font-size: 2.0rem;" class="section-text">
           "...አሁን ሥራችሁን እንዲሁም በፊትም ሆነ አሁን ቅዱሳንን በመርዳት ስለ ስሙ ያሳያችሁትን ፍቅር አይረስም።"
 
           
@@ -190,7 +179,7 @@ customElements.define('aboutus-component', class extends HTMLElement {
         <li class="about-item">
           <ion-icon name="checkmark-circle" aria-hidden="true"></ion-icon>
 
-          <p class="section-text">
+          <p style="font-size: 2.0rem;" class="section-text">
           በተጨማሪም ይህን መልእክት ለሌሎች በማህበራዊ ድህረ ገጾች በማጋራት አብረውን ይተባበሩ። 
           </p>
         </li>
@@ -198,7 +187,7 @@ customElements.define('aboutus-component', class extends HTMLElement {
         <li class="about-item">
           <ion-icon name="checkmark-circle" aria-hidden="true"></ion-icon>
 
-          <p class="section-text">
+          <p style="font-size: 2.0rem;" class="section-text">
           እግዚአብሄር ዘመናችሁን፣ ህይወታችሁን፣ ሥራችሁን፣ አገልግሎታችሁን፣ ቤተሰባችሁን እና ሁለንተናችሁን ይባርክ።
           </p>
         </li>
@@ -524,6 +513,456 @@ customElements.define('gallery-component', class extends HTMLElement {
     </ul>
 </div>
 </section>
+</body>
+  `;
+}
+connectedCallback() {
+  while (this.childNodes.length > 0) {
+      this.shadowRoot.appendChild(this.childNodes[0]);
+  }
+}
+});
+customElements.define('bank-component', class extends HTMLElement {
+  constructor() {
+    super(); // always call super() first in the constructor.
+
+        // Attach a shadow root to <fancy-tabs>.
+  const shadowRoot = this.attachShadow({mode: 'open'});
+  shadowRoot.innerHTML = `
+  <body class="one">
+  <head>
+  <link rel="stylesheet" href="./assets/css/support.css"/>
+  </head>
+  <!-- 
+  - #CATEGORY
+-->
+
+<section  class="section category" aria-label="category">
+  <div class="container">
+
+    <!--<p class="section-subtitle">Course Categories</p>-->
+
+    
+
+    <ul class="grid-list">
+
+      <li>
+        <div class="category-card">
+
+          <div class="card-icon">
+          <img style="max-width:50px;" src="./assets/images/cbe.jpg">
+          </div>
+
+          <div>
+            <h3 class="h3 card-title">
+              <a href="#">1000347718959 (ንግድ ባንክ)</a>
+            </h3>
+
+            <span class="card-meta">Maranata Shimelis</span>
+          </div>
+
+        </div>
+      </li>
+
+      <li>
+        <div class="category-card">
+
+          <div class="card-icon">
+            <img style="min-width: 200px;" src="./assets/images/awash.svg">
+          </div>
+
+          <div>
+            <h3 class="h3 card-title">
+              <a href="#">01320076893600 (አዋሽ) </a>
+            </h3>
+
+            <span class="card-meta">Maranata Shimelis</span>
+          </div>
+
+        </div>
+      </li>
+
+      <li>
+        <div class="category-card">
+
+          <div  class="card-icon">
+            <img  src="./assets/images/Coopbank.svg">
+          </div>
+
+          <div>
+            <h3 class="h3 card-title">
+              <a href="#">1070000012191(Copretiv oromiya)</a>
+            </h3>
+
+            <span class="card-meta">Maranata Shimelis</span>
+          </div>
+
+        </div>
+      </li>
+
+      <li>
+        <div class="category-card">
+
+          <div class="card-icon">
+          <img  src="./assets/images/abyssinia.png">
+          </div>
+
+          <div>
+            <h3 class="h3 card-title">
+              <a href="#">42852333 (አቢሲንያ)</a>
+            </h3>
+
+            <span class="card-meta">Maranata Shimelis</span>
+          </div>
+
+        </div>
+      </li>  
+    </ul>
+
+  </div>
+</section>
+</body>
+  `;
+}
+connectedCallback() {
+  while (this.childNodes.length > 0) {
+      this.shadowRoot.appendChild(this.childNodes[0]);
+  }
+}
+});
+customElements.define('zoom-component', class extends HTMLElement {
+  constructor() {
+    super(); // always call super() first in the constructor.
+
+        // Attach a shadow root to <fancy-tabs>.
+  const shadowRoot = this.attachShadow({mode: 'open'});
+  shadowRoot.innerHTML = `
+  <body class="one">
+  <head>
+  <link rel="stylesheet" href="./assets/css/service.css"/>
+  </head>
+  <main>
+  <article>
+  <section class="section partnership" aria-label="partnership">
+        <div class="container">
+
+          <h2 class="h2 section-title" data-reveal>Want to Join Partnership?</h2>
+
+          <ul class="grid-list">
+
+            <li data-reveal="left">
+              <div class="partnership-card">
+
+                <figure class="card-banner img-holder" style="--width: 640; --height: 402;">
+                  <img src="./assets/images/Zoom1.jpg" width="640" height="402" loading="lazy" alt="Join Courier"
+                    class="img-cover">
+                </figure>
+
+                <div class="card-content">
+                  <h3 class="h5 card-title">Join Courier</h3>
+
+                  <a href="#" class="btn btn-primary has-after">
+                    <span class="span">Learn More</span>
+
+                    <ion-icon name="arrow-forward" aria-hidden="true"></ion-icon>
+                  </a>
+                </div>
+
+              </div>
+            </li>
+
+            <li data-reveal="right">
+              <div class="partnership-card">
+
+                <figure class="card-banner img-holder" style="--width: 640; --height: 402;">
+                  <img src="./assets/images/Zoom2.jpg" width="640" height="402" loading="lazy" alt="Join Merchant"
+                    class="img-cover">
+                </figure>
+
+                <div class="card-content">
+                  <h3 class="h5 card-title">Join Merchant</h3>
+
+                  <a href="#" class="btn btn-primary has-after">
+                    <span class="span">Learn More</span>
+
+                    <ion-icon name="arrow-forward" aria-hidden="true"></ion-icon>
+                  </a>
+                </div>
+
+              </div>
+            </li>
+
+          </ul>
+
+        </div>
+      </section>
+      
+      
+    </article>
+    </main>
+</body>
+  `;
+}
+connectedCallback() {
+  while (this.childNodes.length > 0) {
+      this.shadowRoot.appendChild(this.childNodes[0]);
+  }
+}
+});
+customElements.define('package-component', class extends HTMLElement {
+  constructor() {
+    super(); // always call super() first in the constructor.
+
+        // Attach a shadow root to <fancy-tabs>.
+  const shadowRoot = this.attachShadow({mode: 'open'});
+  shadowRoot.innerHTML = `
+  <body class="one">
+  <head>
+  <link rel="stylesheet" href="./assets/css/package.css"/>
+  </head>
+  <!-- 
+        - #PACKAGE
+      -->
+
+      <section class="package" id="package">
+        <div class="container">
+
+          <p class="section-subtitle">Popular Packeges</p>
+
+          <h2 class="h2 section-title">Join Our Zoom Meetings.</h2>
+
+          
+
+          <ul class="package-list">
+
+            <li>
+              <div class="package-card">
+
+                <figure class="card-banner">
+                  <img src="./assets/images/Zoom1.jpg" alt="Experience The Great Holiday On Beach" loading="lazy">
+                </figure>
+
+                <div class="card-content">
+
+                  <h3 class="h3 card-title">Experience The Great Holiday On Beach</h3>
+
+                  <p class="card-text">
+                    Laoreet, voluptatum nihil dolor esse quaerat mattis explicabo maiores, est aliquet porttitor! Eaque,
+                    cras, aspernatur.
+                  </p>
+
+                  <ul class="card-meta-list">
+
+                    <li class="card-meta-item">
+                      <div class="meta-box">
+                        <ion-icon name="time"></ion-icon>
+
+                        <p class="text"> 3:00 PM</p>
+                      </div>
+                    </li>
+
+                    
+
+                    <li class="card-meta-item">
+                      <div class="meta-box">
+                        <ion-icon name="location"></ion-icon>
+
+                        <p class="text">Washington DC</p>
+                      </div>
+                    </li>
+
+                  </ul>
+                  <ul class="card-meta-list">
+
+                  <li class="card-meta-item">
+                    <div class="meta-box">
+                      <ion-icon name="time"></ion-icon>
+
+                      <p class="text">3:00 AM</p>
+                    </div>
+                  </li>
+
+                  
+
+                  <li class="card-meta-item">
+                    <div class="meta-box">
+                      <ion-icon name="location"></ion-icon>
+
+                      <p class="text">Toronto</p>
+                    </div>
+                  </li>
+
+                </ul>
+                <ul class="card-meta-list">
+
+                <li class="card-meta-item">
+                  <div class="meta-box">
+                    <ion-icon name="time"></ion-icon>
+
+                    <p class="text">9:00 PM</p>
+                  </div>
+                </li>
+
+               
+
+                <li class="card-meta-item">
+                  <div class="meta-box">
+                    <ion-icon name="location"></ion-icon>
+
+                    <p class="text">Ethiopia</p>
+                  </div>
+                </li>
+
+              </ul>
+
+                </div>
+
+                <div class="card-price">
+
+                  <div class="wrapper">
+
+                    
+
+                  </div>
+
+                  <p class="price">
+                  ID 6206841297
+                  
+                    <span>Passcode PMS</span>
+                  </p>
+
+                  <button class="btn btn-secondary">Join Meeting</button>
+
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="package-card">
+
+                <figure class="card-banner">
+                  <img src="./assets/images/Zoom2.jpg" alt="Summer Holiday To The Oxolotan River" loading="lazy">
+                </figure>
+
+                <div class="card-content">
+
+                  <h3 class="h3 card-title">Summer Holiday To The Oxolotan River</h3>
+
+                  <p class="card-text">
+                    Laoreet, voluptatum nihil dolor esse quaerat mattis explicabo maiores, est aliquet porttitor! Eaque,
+                    cras, aspernatur.
+                  </p>
+
+                  <ul class="card-meta-list">
+
+                    <li class="card-meta-item">
+                      <div class="meta-box">
+                        <ion-icon name="time"></ion-icon>
+
+                        <p class="text">7D/6N</p>
+                      </div>
+                    </li>
+
+                    <li class="card-meta-item">
+                      <div class="meta-box">
+                        <ion-icon name="people"></ion-icon>
+
+                        <p class="text">pax: 10</p>
+                      </div>
+                    </li>
+
+                    <li class="card-meta-item">
+                      <div class="meta-box">
+                        <ion-icon name="location"></ion-icon>
+
+                        <p class="text">Malaysia</p>
+                      </div>
+                    </li>
+
+                  </ul>
+                  <ul class="card-meta-list">
+
+                    <li class="card-meta-item">
+                      <div class="meta-box">
+                        <ion-icon name="time"></ion-icon>
+
+                        <p class="text">7D/6N</p>
+                      </div>
+                    </li>
+
+                    <li class="card-meta-item">
+                      <div class="meta-box">
+                        <ion-icon name="people"></ion-icon>
+
+                        <p class="text">pax: 10</p>
+                      </div>
+                    </li>
+
+                    <li class="card-meta-item">
+                      <div class="meta-box">
+                        <ion-icon name="location"></ion-icon>
+
+                        <p class="text">Malaysia</p>
+                      </div>
+                    </li>
+
+                  </ul>
+                  <ul class="card-meta-list">
+
+                    <li class="card-meta-item">
+                      <div class="meta-box">
+                        <ion-icon name="time"></ion-icon>
+
+                        <p class="text">7D/6N</p>
+                      </div>
+                    </li>
+
+                    <li class="card-meta-item">
+                      <div class="meta-box">
+                        <ion-icon name="people"></ion-icon>
+
+                        <p class="text">pax: 10</p>
+                      </div>
+                    </li>
+
+                    <li class="card-meta-item">
+                      <div class="meta-box">
+                        <ion-icon name="location"></ion-icon>
+
+                        <p class="text">Malaysia</p>
+                      </div>
+                    </li>
+
+                  </ul>
+
+                </div>
+
+                <div class="card-price">
+
+                 
+
+                  <p class="price">
+                  ID 6206841297
+                  
+                  <span>Passcode PMS</span>
+                  </p>
+
+                  <button class="btn btn-secondary">Join Meeting</button>
+
+                </div>
+
+              </div>
+            </li>
+
+           
+          </ul>
+
+          
+          <h2 class="h2 section-title">Check Our Youtube Channel</h2>
+        </div>
+      </section>
+      
 </body>
   `;
 }
